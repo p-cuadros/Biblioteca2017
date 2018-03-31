fc7944e3761acae22b2d79bd885e26b5f3fd53e9
ADOCENTE <ADOCENTE@ADOCENTE>
2018-03-31 12:15:14 -0500

    Agregando el contecto

---
+++ Biblioteca2017.Infraestructura.Datos/BibliotecaContexto.cs
@@ -0,0 +1,22 @@
+﻿using System.Data.Entity;
+
+namespace Biblioteca2017.Infraestructura.Datos
+{
+    public class BibliotecaContexto : DbContext
+    {
+        public BibliotecaContexto() : base("biblio")
+        {
+        }
+        public DbSet<Libro> Libros { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Valoracion> Valoraciones { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Configurations.Add(new LibroMapeo());
+            modelBuilder.Configurations.Add(new UsuarioMapeo());
+            modelBuilder.Configurations.Add(new ValoracionMapeo());
+        }
+    }
+}
