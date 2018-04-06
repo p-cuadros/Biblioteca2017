fa4eaba4dc4beecfe3158e040d386cc9d401d6f5
ADOCENTE <ADOCENTE@ADOCENTE>
2018-04-05 21:16:37 -0500

    Adicionar repositorio

---
+++ Biblioteca2017.Infraestructura.Datos/Repositorio.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca2017.Infraestructura.Datos
+{
+    public class Repositorio : IRepositorio
+    {
+        BibliotecaContexto ioContexto;
+        public Repositorio()
+        {
+            ioContexto = new BibliotecaContexto();
+        }
+
+        public void Adicionar<T>(T aoObjeto) where T : class
+        {
+            ioContexto.Set<T>().Add(aoObjeto);
+        }
+
+        public void GuardarCambios() 
+        {
+            ioContexto.SaveChanges();
+        }
+
+        public IList<T> Listar<T>() where T : class
+        {
+            return ioContexto.Set<T>().ToList();
+        }
+
+        public T ObtenerPorCodigo<T>(params object[] aoLlaves) where T : class
+        {
+            return ioContexto.Set<T>().Find(aoLlaves);
+        }
+    }
+}
---
+++ Biblioteca2017/IRepositorio.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca2017
+{
+    public interface IRepositorio
+    {
+        T ObtenerPorCodigo<T>(params object[] aoLlaves) where T : class;
+        IList<T> Listar<T>() where T : class;
+        void Adicionar<T>(T aoObjeto) where T : class;
+        void GuardarCambios();
+    }
+}
